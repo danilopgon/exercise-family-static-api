99d252f2f65507217c0178127c64714fd48cc80c
danilopgon <113547781+danilopgon@users.noreply.github.com>
2023-06-22 18:52:00 +0000

    Modify to use id

--- src/datastructures.py
+++ src/datastructures.py
@@ -1,4 +1,3 @@
-
 """
 update this file to implement the following already declared methods:
 - add_member: Should add a member to the self._members list
@@ -12,25 +11,32 @@ from random import randint
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
-
         # example list of members
-        self._members = []
+        self._members = [
+            {"id": self._generateId(), "first_name": "John", "last_name": last_name}
+        ]
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
         return randint(0, 99999999)
 
     def add_member(self, member):
-
-        pass
+        current_members = self._members[:]
+        new_member = {
+            "id": self._generateId(),
+            "first_name": member,
+            "last_name": self.last_name,
+        }
+        current_members.append(new_member)
+        self._members = current_members
 
     def delete_member(self, id):
-        # fill this method and update the return
-        pass
+        member = [member for member in self._members if member["id"] == id]
+        self._members.remove(member[0])
 
     def get_member(self, id):
-        # fill this method and update the return
-        pass
+        member = [member for member in self._members if member["id"] == id]
+        return member[0]
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
